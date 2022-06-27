ae7f617e597a9cd8c7033059162e57fae4b08ce4
Addyani Amirul Fajar <99247693+addyani@users.noreply.github.com>
2022-06-27 18:48:29 +0700

    Update Title.js

--- src/components/Title.js
+++ src/components/Title.js
@@ -3,7 +3,8 @@ import React from "react";
 export default function Title() {
   return (
     <div className="title">
-      <h1>Todo App</h1>
+      <h1>Todo List App</h1>
+      <h3>With React CRUD</h3>
     </div>
   );
 }
